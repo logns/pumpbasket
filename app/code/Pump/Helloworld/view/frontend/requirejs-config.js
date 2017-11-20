d217f760209bc8f684398b5394b8bf091ab14bfb
Poornima Sharma <lognsystems@gmail.com>
2017-11-20 10:54:01 +0530

    custom module with alert

---
+++ app/code/Pump/Helloworld/view/frontend/requirejs-config.js
@@ -0,0 +1,14 @@
+var config = {
+    map: {
+        '*': {
+            mycomponent: 'Pump_Helloworld/js/',
+        }
+    
+//    shim: {
+//             'Test': {
+//                  deps: ['jquery']
+//              }
+//       } 
+    
+    }
+};
